dca68dcdd439d797d2550f46170f3487a40a6bbf
Hrithik Sharma <hrithikpvkr@gmail.com>
2025-03-30 15:25:15 +0530

    Updated next.config.js to fix image loading issue

--- next.config.js
+++ next.config.js
@@ -14,11 +14,37 @@
 
 // module.exports = nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: "/crise.io", // Replace with your GitHub repo name
+//   trailingSlash: true,
+// };
+
+// module.exports = nextConfig;
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     loader: "default",
+//     path: isProd ? "/crise.io/" : "/",
+//   },
+// };
+
+
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: "/crise.io", // Replace with your GitHub repo name
-  trailingSlash: true,
+  reactStrictMode: true,
+  output: "export",
+  basePath: isProd ? "/crise.io" : "",
+  assetPrefix: isProd ? "/crise.io/" : "",
+  images: {
+    unoptimized: true, // This is important for Next.js static exports (GitHub Pages)
+  },
 };
 
 module.exports = nextConfig;
