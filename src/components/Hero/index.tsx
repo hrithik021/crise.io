c7e1765fe8e48ea6c2bf0bbd9c991f5dd9a19309
Hrithik Sharma <hrithikpvkr@gmail.com>
2025-07-01 22:55:03 +0530

    Update Hero component 2

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,27 +13,19 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-                {/* Logo */}
-                <div className="mb-6 flex justify-center">
-                  <img
-                    src="/images/logo/logo-2.svg"
-                    alt="Crise.io Logo"
-                    className="h-16 w-auto"
-                  />
-                </div>
-
                 <h5 className="mb-2 text-2xl font-bold leading-tight text-black dark:text-white sm:text-1xl sm:leading-tight md:text-4xl md:leading-tight">
-                  Unlock the Power of Fight Data with AI
+                
+                 Unlock the Power of Fight Data with AI
                 </h5>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Crise.io is a cutting-edge platform revolutionizing the combat sports industry by integrating advanced technology, data analytics, and AI-driven insights to elevate training, performance, and fan engagement.
+                Crise.io is a cutting-edge platform revolutionizing the combat sports industry by integrating advanced technology, data analytics, and AI-driven insights to elevate training, performance, and fan engagement.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="mailto:miguel.ridj@gmail.com"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Request a Demo🔥
+                    Request a Demo🔥 
                   </Link>
                   <Link
                     href="mailto:miguel.ridj@gmail.com"
